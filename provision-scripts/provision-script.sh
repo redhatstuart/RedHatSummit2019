bf4a766f85dd55c289686614bd15ae5e04d8ef0c
Stuart Kirk <stuart.kirk@microsoft.com>
2019-03-19 01:38:48 -0400

    Provisioning Scripts

--- provision-scripts/provision-script.sh
+++ provision-scripts/provision-script.sh
@@ -47,13 +47,6 @@ echo "**************************************************************************
 	sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
 	yum install azure-cli >> /root/yum-output.log
 echo "********************************************************************************************"	
-	echo "`date` -- Upgrading PIP and installing Ansible" >>/root/provision-script-output.log
-	pip install --upgrade pip >> /root/pip-output.log
-	pip install --upgrade python-dateutil >> /root/pip-output.log
-	yum -y remove pyOpenSSL rhn-check rhn-client-tools rhn-setup rhn-setup-gnome rhnlib rhnsd yum-rhn-plugin >>/root/yum-output.log
-	pip install pyOpenSSL >> /root/pip-output.log
-	pip install ansible==2.7.9 >> /root/pip-output.log
-echo "********************************************************************************************"
 	echo "`date` -- Adding package elements to enable graphical interface" >>/root/provision-script-output.log
 	yum -y groupinstall "Server with GUI" >> /root/yum-output.log
 echo "********************************************************************************************"
@@ -76,6 +69,15 @@ echo "**************************************************************************
         systemctl enable websockify.service
         systemctl start vncserver@:4.service
 	systemctl start websockify.service
+echo "********************************************************************************************"
+        echo "`date` -- Upgrading PIP and installing Ansible" >>/root/provision-script-output.log
+        pip install --upgrade pip >> /root/pip-output.log
+        pip install --upgrade python-dateutil >> /root/pip-output.log
+        yum -y remove pyOpenSSL rhn-check rhn-client-tools rhn-setup rhn-setup-gnome rhnlib rhnsd yum-rhn-plugin >>/root/yum-output.log
+        pip install pyOpenSSL >> /root/pip-output.log
+        pip install ansible==2.7.9 >> /root/pip-output.log
+echo "********************************************************************************************"
+
 echo "`date` --END-- Provisioning" >>/root/provision-script-output.log
 
 echo "`date` Creating Student Desktop Credentials File" >>/root/provision-script-output.log
