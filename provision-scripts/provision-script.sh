10c3d1fafe84ab481e44056633181283ca306640
Stuart Kirk <stuart.kirk@microsoft.com>
2019-03-18 22:51:05 -0400

    Provisioning Scripts

--- provision-scripts/provision-script.sh
+++ provision-scripts/provision-script.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+AZ_USER_NAME=${1}
+AZ_USER_PASSWORD=${2}
+AZ_TENANT_ID=${3}
+AZ_SUBSCRIPTION_ID=${4}
+SP_NAME=${5}
+SP_SECRET=${6}
+SP_OBJECT_ID=${7}
+SP_APP_ID=${8}
+
 echo "`date` --BEGIN-- Provision Stage 1 Script" >>/root/lsprovision.log
 echo "********************************************************************************************"
 	echo "`date` -- Setting Student User password to 'Microsoft'" >>/root/lsprovision.log
@@ -51,3 +60,15 @@ echo "**************************************************************************
         systemctl start vncserver@:4.service
 	systemctl start websockify.service
 echo "`date` --END-- Provision Stage 1 Script" >>/root/lsprovision.log
+
+echo AZ_USER_NAME=$AZ_USER_NAME >> /home/student/Desktop/credentials.txt
+echo AZ_USER_PASSWORD=$AZ_USER_PASSWORD >> /home/student/Desktop/credentials.txt
+echo AZ_TENANT_ID=$AZ_TENANT_ID >> /home/student/Desktop/credentials.txt
+echo AZ_SUBSCRIPTION_ID=$AZ_SUBSCRIPTION_ID >> /home/student/Desktop/credentials.txt
+echo SP_NAME=$SP_NAME >> /home/student/Desktop/credentials.txt
+echo SP_SECRET=$SP_SECRET >> /home/student/Desktop/credentials.txt
+echo SP_OBJECT_ID=$SP_OBJECT_ID >> /home/student/Desktop/credentials.txt
+echo SP_APP_ID=$SP_APP_ID >> /home/student/Desktop/credentials.txt
+echo GUIDE_URL=https://github.com/stuartatmicrosoft/Azure-Linux-Migration-Workshop >> /home/student/Desktop/credentials.txt
+chown student:student /home/student/Desktop/credentials.txt
+
