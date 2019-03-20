35d2e3ec868d1b158a412ef30ae58e4a84df9c88
Stuart Kirk <stuart.kirk@microsoft.com>
2019-03-20 11:16:23 -0400

    Provisioning Scripts

--- provision-scripts/provision-script.sh
+++ provision-scripts/provision-script.sh
@@ -94,9 +94,9 @@ echo AZ_USER_NAME=$AZ_USER_NAME >> /home/student/Desktop/credentials.txt
 echo AZ_USER_PASSWORD=$AZ_USER_PASSWORD >> /home/student/Desktop/credentials.txt
 echo AZ_TENANT_ID=$AZ_TENANT_ID >> /home/student/Desktop/credentials.txt
 echo AZ_SUBSCRIPTION_ID=$AZ_SUBSCRIPTION_ID >> /home/student/Desktop/credentials.txt
-echo SP_NAME=$SP_NAME >> /home/student/Desktop/credentials.txt
+#echo SP_NAME=$SP_NAME >> /home/student/Desktop/credentials.txt
 echo SP_SECRET=$SP_SECRET >> /home/student/Desktop/credentials.txt
-echo SP_OBJECT_ID=$SP_OBJECT_ID >> /home/student/Desktop/credentials.txt
+#echo SP_OBJECT_ID=$SP_OBJECT_ID >> /home/student/Desktop/credentials.txt
 echo SP_APP_ID=$SP_APP_ID >> /home/student/Desktop/credentials.txt
 echo GUIDE_URL=https://github.com/stuartatmicrosoft/RedHatSummit2019 >> /home/student/Desktop/credentials.txt
 chown student:student /home/student/Desktop/credentials.txt
