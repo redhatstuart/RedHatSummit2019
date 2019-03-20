57936c45b91ce651bf4c7d1f64082b32f1ddb8a2
Stuart Kirk <stuart.kirk@microsoft.com>
2019-03-20 15:23:16 -0400

    Provisioning Scripts

--- provision-scripts/provision-script.sh
+++ provision-scripts/provision-script.sh
@@ -73,7 +73,7 @@ echo "**************************************************************************
         echo "`date` -- Upgrading PIP and installing Ansible" >>/root/provision-script-output.log
         pip install --upgrade pip >> /root/pip-output.log
         pip install --upgrade python-dateutil >> /root/pip-output.log
-        yum -y remove pyOpenSSL rhn-check rhn-client-tools rhn-setup rhn-setup-gnome rhnlib rhnsd yum-rhn-plugin >>/root/yum-output.log
+        yum -y remove pyOpenSSL rhn-check rhn-client-tools rhn-setup rhn-setup-gnome rhnlib rhnsd yum-rhn-plugin PackageKit* >>/root/yum-output.log
         pip install pyOpenSSL >> /root/pip-output.log
         pip install ansible==2.7.9 >> /root/pip-output.log
 echo "********************************************************************************************"
